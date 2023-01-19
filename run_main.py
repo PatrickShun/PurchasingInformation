1dc69d0a25040f103403e2c22d58ad3ee29fa291
PatrickShun <torrey@126.com>
2023-01-19 17:50:25 +0800

    2023-01-19-PM

--- run_main.py
+++ run_main.py
@@ -54,9 +54,13 @@ class PurchasingInfo:
         parseHtml = etree.HTML(html)
         self.cityName, self.home_links, self.release_date, self.page_title, self.publisher = [],[],[],[],[]
         self.home_links = parseHtml.xpath(self.conf['position']['HomeLinks'])
+        self.home_links = [li.strip() for li in self.home_links]
         self.release_date = parseHtml.xpath(self.conf['position']['ReleaseDate'])
+        self.release_date = [dt.strip() for dt in self.release_date]
         self.page_title = parseHtml.xpath(self.conf['position']['PageTitle'])
+        self.page_title = [ti.strip() for ti in self.page_title]
         self.publisher = parseHtml.xpath(self.conf['position']['Publisher'])
+        self.publisher = [ps.strip() for ps in self.publisher]
 
 
     def writeData(self,cityName):
@@ -64,15 +68,10 @@ class PurchasingInfo:
         if len(self.home_links) == len(self.page_title):
             wb = EditExcel.runExcel(cityName)
             wb.writeExcel(self.cityName, 1)
-            print("25%...")
             wb.writeExcel(self.release_date, 2)
-            print("50%...")
             wb.writeExcel(self.page_title, 3)
-            print("75%...")
             wb.writeExcel(self.publisher, 4)
-            print("99%...")
             wb.writeExcel(self.home_links, 5)
-            print('100%...')
 
     def readData(self,cityName,col):
         wb_r = EditExcel.runExcel(cityName)
@@ -80,29 +79,37 @@ class PurchasingInfo:
         [dataList.append(i.value) for i in wb_r.readExcel(col)]
         return dataList
 
-
-
+    def compareData(self, cityName):
+        # 获取原始数据的标题，用于比较最新获取到的标题
+        rawData_title = set(self.readData(cityName,'C'))
+        # 转换获取到的最新数据
+        newData_title = set(self.page_title)
+        # 计算差值
+        difference_title = newData_title - rawData_title
+        # 如果有差值，。。。
+        if difference_title:
+            [print(i) for i in difference_title]
+        else:
+            print('Nope')
+        return  difference_title
 
 
     def irun(self):
         # 1.获取json的所有城市;
-        # with open('cityName.json','r') as load_js:
-        #     self.load_dict = json.load(load_js)
+        with open('cityName.json','r') as load_js:
+            self.load_dict = json.load(load_js)
 
         # 2.遍历字典中的所有城市；
         # for cityName in self.load_dict.keys():
         #     print('-' * 20)
         #     print(cityName + '...')
 
-            # 3.Get页面之前需要保存历史数据（当前get的城市）
-        rawData_time = set(self.readData('石家庄','B'))
-        rawData_title = set(self.readData('石家庄','C'))
-
-            # 4.获取主页的第一页的所有子URL，也就是50个详细URL结果；
+            # 4.获取主页的第一页的所有子URL，也就是50个详细URL结果, 并且保存标题等信息
             # self.getHomeLinks(self.getHTML(self.load_dict[cityName]))
         self.getHomeLinks(self.getHTML('石家庄'))
 
             # 5.使用历史数据与最新数据作并集（|），并集后保存到需要写入的列表中；
+        self.compareData('石家庄')
 
             # 6.使用历史数据与最新数据作求差值（-），用于提醒与标记新增的内容；
 
@@ -110,14 +117,6 @@ class PurchasingInfo:
             # self.writeData(cityName)
 
             # 8.针对新增内容执行提醒与标记；
-
-        print(rawData_time)
-        print(rawData_title)
-        l1 = set(self.release_date)
-        print(l1)
-        # l1 = rawData_time - rawData_title
-
-
 
 
 
